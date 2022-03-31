747dfb2f239269272330f6014b3b9bac727e8732
YixingWang1999 <84959521+YixingWang1999@users.noreply.github.com>
2022-03-31 00:21:52 -0400

    Update lambda_function.py

--- search-photos-copy/lambda_function.py
+++ search-photos-copy/lambda_function.py
@@ -8,6 +8,9 @@ import boto3
 from opensearchpy import OpenSearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import json
+import botocore
+import base64
+# import botocore.exceptions
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -46,26 +49,57 @@ def search_photos(class_type):
     )
         
     q = class_type
-    query = {
-      'query': {
-        'match': {
-          'labels': q
-        }
-      }
-    }
-    response = client.search(
-        body = query,
-        index = 'photos'
-    )
     
-    print('\nSearch results:')
-    print(response)
+    q = q.split('and ')
     
     class_data = []
-    for item in response['hits']['hits']:
-        key, bucket = item['_source']['objectKey'], item['_source']['bucket']
-        class_data.append((key, bucket))
+    
+    for item in q:
+        if 'bird' in item:
+            item = 'bird'
+        elif 'tree' in item:
+            item = 'tree'
+        query = {
+          'query': {
+            'match': {
+              'labels': item
+            }
+          }
+        }
+        response = client.search(
+            body = query,
+            index = 'photos'
+        )
+        for item in response['hits']['hits']:
+            key, bucket = item['_source']['objectKey'], item['_source']['bucket']
+            class_data.append((key, bucket))
+    # if 'bird' in q and 'tree' in q:
+    #     q = ['bird', 'tree']
+    # elif 'bird' in q:
+    #     q = 'bird'
+    # elif 'tree' in q:
+    #     q = 'tree'
+    # query = {
+    #   'query': {
+    #     'match': {
+    #       'labels': q
+    #     }
+    #   }
+    # }
+    # response = client.search(
+    #     body = query,
+    #     index = 'photos'
+    # )
+    
+    # print('\nSearch results:')
+    # print(response)
+    
+    # class_data = []
+    # for item in response['hits']['hits']:
+    #     key, bucket = item['_source']['objectKey'], item['_source']['bucket']
+    #     class_data.append((key, bucket))
     print(class_data)
+    return class_data
 
 def dispatch(intent_request):
     
@@ -93,26 +127,76 @@ def dispatch(intent_request):
 # Define the client to interact with Lex
 # client = boto3.client('lex-runtime')
 def lambda_handler(event, context):
-    
-
     os.environ['TZ'] = 'America/New_York'
     time.tzset()
+    client = boto3.client('lex-runtime')
     # logger.debug('event.bot.name={}'.format(event['bot']['name']))
     
     transaction_type = event['queryStringParameters']['type']
     print(transaction_type)
+    
     transaction_response = {
         'transactionType': transaction_type
     }
     
+    class_data = search_photos(transaction_type)
+    # https://yw3747-b2.s3.amazonaws.com/1200.jpeg
+    img_url = []
+    for item in class_data:
+        s = 'https://' + str(item[1]) + '.s3.amazonaws.com/' + str(item[0])
+        img_url.append(s)
+    # s3 = boto3.client('s3')
+    # for item in class_data:
+    #     img = s3.get_object(Bucket = item[1], Key = item[0])
+    #     img_files.append(img)
+    #     #img_content = img['Body'].read()
+    # print(img_files)
+    # img_content = img_files[0]['Body'].read()
     response = {}
     response['statusCode'] = 200
     response['headers'] = {}
     response['headers']['Content-Type'] = 'application/json'
-    response['body'] = json.dumps(transaction_response)
+    response['body'] = json.dumps(img_url)
+    print(response)
+    # response['headers']['Content-Type'] = 'application/jpeg'
+    # response['headers']['Content-Dispostion'] = 'attachment; filename={}'.format(class_data[0][0])
+    # response['body'] = base64.b64encode(img_content)
+    # response['isBase64Encoded'] = True
     
     return response
+# try:
+    # responderName = event["DestinationBot"]
+    # userId = event["RecipientID"]
+    # userInput = event["message"]["text"]
+
+    # client = boto3.client('lex-runtime')
+
+    # response = client.post_text(
+    #     botName=responderName,
+    #     botAlias=responderName,
+    #     userId=userId,
+    #     sessionAttributes={
+    #     },
+    #     requestAttributes={
+    #     },
+    #     inputText= userInput
+    # )
+    # class_type = 'show me bird'
+
     
+    # # print('client', client)
+    # try:
+    #     response = client.post_text(botName='photo',
+    #                             botAlias='pt',
+    #                             # userId='testuser',
+    #                             inputText=str(class_type))
+    #     print('response', response)
+    #     return response
+    
+    # except botocore.exceptions as e:
+    #     print('error', e.response['Error']['Message'])
+    
+
     # return dispatch(event)
     # print(event)
     # try:
@@ -139,7 +223,7 @@ def lambda_handler(event, context):
 #     print("msg_from_user is: ", msg_from_user)
 #     response = client.post_text(botName='chatbot',
 #                                 botAlias='chat',
-#                                 userId='testuser',
+#                                 uswoerId='testuser',
 #                                 inputText=msg_from_user)
     
     
